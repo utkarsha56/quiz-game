8a89f737a65c09adc4506fa7c33087e0033f6b7d
utkarsha56 <utkarshachakrabortty56@gmail.com>
2024-11-10 23:01:36 +0530

    Update result.js

--- result.js
+++ result.js
@@ -3,7 +3,10 @@ const response = document.querySelector('.response .text')
 const score = Number(localStorage.getItem('CurrentScore'));
 const heighestscore = Number(localStorage.getItem('HighestScore'));
 
-
+localStorage.setItem('CurrentScore', '0')
+localStorage.q1visited = localStorage.q2visited = localStorage.q3visited = localStorage.q4visited = localStorage.q5visited = localStorage.q6visited = localStorage.q7visited = localStorage.q8visited = localStorage.q9visited = localStorage.q10visited = 'false';
+localStorage.setItem('pageno', '0')
+localStorage.setItem('isplayed', 'true')
 
 if(score >=9){
     response.innerHTML = `"Great"`
@@ -16,14 +19,6 @@ if(score >=9){
 if(score>heighestscore){
     localStorage.setItem('HighestScore', `${score}`)
 }
-
-restart.addEventListener('click',function(){
-    localStorage.setItem('CurrentScore', '0')
-    localStorage.q1visited = localStorage.q2visited = localStorage.q3visited = localStorage.q4visited = localStorage.q5visited = localStorage.q6visited = localStorage.q7visited = localStorage.q8visited = localStorage.q9visited = localStorage.q10visited = 'false';
-    localStorage.setItem('pageno', '0')
-    localStorage.setItem('isplayed', 'true')
-})
-
 
 
 // --------------------------- progressbar logic ----------------------
