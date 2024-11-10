2a09c0ec79091ee6deb93a214614ae1fbb605d9c
utkarsha56 <utkarshachakrabortty56@gmail.com>
2024-11-10 22:09:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -48,6 +48,8 @@ deletebtn.addEventListener('click', function(){
     localStorage.setItem('pageno', '0')
     localStorage.q1visited = localStorage.q2visited = localStorage.q3visited = localStorage.q4visited = localStorage.q5visited = localStorage.q6visited = localStorage.q7visited = localStorage.q8visited = localStorage.q9visited = localStorage.q10visited = 'false';
     deletenotification.style.display = "block"
+    startbtn.innerHTML = `Start Now >>>`
+    nextbtn.href = "question1.html"
     scorebord.innerHTML = 'New Player'
     setTimeout(() => {
         deletenotification.style.display = "none"
